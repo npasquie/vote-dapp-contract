01adb1ffc1955079e4f09bf0ba562565d51409ea
npasquie <nicolas.pasquier.pro@gmail.com>
2020-04-06 17:50:52 +0200

    contract done, untested

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -6,9 +6,9 @@ contract Ballot {
     bytes32 name;
     bytes32 question;
     uint endTime; //time after which no vote can be registered anymore, based on unix epoch
-    mapping (bytes32 => bool) m_voters; //hash of the voter's secret code => status (voted or not)
-    bool externalities_enabled; //to create ballots without controversial externalities
-    uint number_of_candidates; //actually useful
+    mapping (bytes32 => bool) m_voterHasVotedWithCodeHash; //hash of the voter's secret code => status (voted or not)
+    bool externalitiesEnabled; //to create ballots without controversial externalities
+    uint numberOfCandidates; //actually useful
     mapping (uint => s_candidate) m_candidates; //id to candidates
     mapping (uint => uint) m_candidate_poll; //candidate id to his number of votes
     mapping (uint => uint) m_candidate_externality; /* candidate id to the sum of his externalities,
@@ -20,29 +20,108 @@ contract Ballot {
         bytes32 pictureHash; //optional
     }
 
-    constructor(bytes32 _name, bytes32 _question, uint _endTime, bytes32[] _voters_hashes, bool _externalities_enabled,
+    constructor(bytes32 _name, bytes32 _question, uint _endTime, bytes32[] _votersHashes, bool _externalitiesEnabled,
         s_candidate[] _candidates) public {
-        require(_endTime > block.timestamp, "end time of the ballot is in the past");
-        require(_voters_hashes.length >= 2, "not ballot can be created with less than 2 candidates");
-
-        uint
+        require(! _name[0] == 0 && ! _question[0] == 0, "name and question must be defined");
+        require(_endTime > now, "end time of the ballot must be in the future");
+        require(_votersHashes.length >= 2, "no ballot can be created with less than 2 candidates");
+        require(! containsIdenticalNames(_candidates), "every candidate's names must be distinct");
+        require(! containsIdenticalValues(_votersHashes), "each voter's hashes must bes distinct");
 
         owner = msg.sender;
         name = _name;
         question = _question;
-        for (uint i = 0; i < _voters_hashes.length; i++){
-            m_voters[_voters_hashes[i]] = false;
-        }
-        externalities_enabled = _externalities_enabled;
-        number_of_candidates = 0; //used as iterator, last value after last while loop will be correct
-        while (number_of_candidates < _candidates.length){
-            require(_candidates[number_of_candidates].name != 0, "every candidate must have a defined name");
+        endTime = _endTime;
+        externalitiesEnabled = _externalitiesEnabled;
+        numberOfCandidates = 0; //used as iterator, last value after last while loop will be correct
+        while (numberOfCandidates < _candidates.length){
+            require(_candidates[numberOfCandidates].name[0] != 0, "every candidate must have a defined name");
 
-            m_candidates[number_of_candidates] = _candidates[number_of_candidates];
-            number_of_candidates++;
+            m_candidates[numberOfCandidates] = _candidates[numberOfCandidates];
+            numberOfCandidates++;
         }
     }
 
     //contract core code
 
+    //modifiers
+    modifier onlyOwner(){
+        require(msg.sender == owner, "function reserved to the contract owner");
+    }
+
+    modifier mustBeBeforeEndTime(){
+        require(now < endTime, "ballot must still be open");
+    }
+
+    modifier checksCandidateId(uint _candidateId){
+        require(_candidateId < numberOfCandidates, "candidate id must exist");
+    }
+
+    // functions used in constructor checks
+    function containsIdenticalNames(s_candidate[] memory _candidates) private pure returns(bool){
+        mapping (bytes32 => bool) memory m_names;
+
+        for(uint i = 0; i < _candidates.length; i++){
+            if(m_names[_candidates[i].name]){
+                return true;
+            }
+            m_names[_candidates[i].name] = true;
+        }
+        return false;
+    }
+
+    function containsIdenticalValues(bytes32[] memory values) private pure returns(bool){
+        mapping (bytes32 => bool) memory m_valueIsUsed;
+
+        for(uint i = 0; i < values.length; i++){
+            if(m_valueIsUsed[values[i]]){
+                return true;
+            }
+            m_valueIsUsed[values[i]] = true;
+        }
+        return false;
+    }
+
+    //public methods
+    //getters
+    function getCandidateScore(uint _candidateId) public view returns(uint){
+        return m_candidate_poll[_candidateId] + m_candidate_externality[_candidateId];
+    }
+
+    function getCandidateExternality(uint _candidateId) public view returns(uint){
+        return m_candidate_externality[_candidateId];
+    }
+
+    /* in front, use this value to request every candidate score
+    no function "getAllCandidates" is implemented to save gas */
+    function getNumberOfCandidates() public view returns(uint){
+        return numberOfCandidates;
+    }
+
+    function getEndTime() public view returns(uint){
+        return endTime;
+    }
+
+    function getName() public view returns(bytes32){
+        return name;
+    }
+
+    function getQuestion() public view returns(bytes32){
+        return question;
+    }
+
+    //for voters
+    function vote(uint _candidateId, bytes32 _voter_code) checksCandidateId mustBeBeforeEndTime public{
+        require(! m_voterHasVotedWithCodeHash[keccak256(_voter_code)], "you can only vote once");
+
+        m_candidate_poll[_candidateId]++;
+        m_voterHasVotedWithCodeHash[keccak256(_voter_code)] = true;
+    }
+
+    //for organiser
+    function addNewExternality(uint _candidateId, uint _externality) checksCandidateId onlyOwner public{
+        require(externalitiesEnabled, "externalities must be enabled at ballot creation");
+
+        m_candidate_externality[_candidateId] += _externality;
+    }
 }
