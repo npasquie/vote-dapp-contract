a0f6b910ed252d83b588b9fb45a030cccfeeb897
npasquie <nicolas.pasquier.pro@gmail.com>
2020-04-07 14:53:25 +0200

    made 80 caracters norm compliant

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -14,11 +14,12 @@ contract Ballot {
     address owner;
     bytes32 name;
     bytes32 question;
-    uint endTime; //time after which no vote can be registered anymore, based on unix epoch
-    mapping (bytes32 => bool) m_voterHasNotYetVotedWithCodeHash;/* turns true in constructor for allowed voters,
-    false after vote */
-
-    bool externalitiesEnabled; //to create ballots without controversial externalities
+    uint endTime; /* time after which no vote can be registered anymore,
+    based on unix epoch */
+    mapping (bytes32 => bool) m_voterHasNotYetVotedWithCodeHash;/* turns true in
+    constructor for allowed voters, false after vote */
+    bool externalitiesEnabled; /* to create ballots without controversial
+    externalities */
     bytes32[] candidateNames;
     mapping (bytes32 => s_candidate) m_candidates;
 
@@ -29,12 +30,21 @@ contract Ballot {
         bytes32 pictureHash; //optional
     }
 
-    //warning : if many candidates with the same name are sent, last one will overwrite the others
-    constructor(bytes32 _name, bytes32 _question, uint _endTime, bytes32[] memory _votersCodeHashes,
-        bool _externalitiesEnabled, bytes32[] memory _pictureHashes, bytes32[] memory _candidateNames) public {
-        require(! (_name[0] == 0) && ! (_question[0] == 0), "name and question must be defined");
+    /* warning : if many candidates with the same name are sent, last one
+    will overwrite the others */
+    constructor(
+        bytes32 _name,
+        bytes32 _question,
+        uint _endTime,
+        bytes32[] memory _votersCodeHashes,
+        bool _externalitiesEnabled,
+        bytes32[] memory _pictureHashes,
+        bytes32[] memory _candidateNames) public {
+        require(! (_name[0] == 0) && ! (_question[0] == 0),
+          "name and question must be defined");
         require(_endTime > now, "end time of the ballot must be in the future");
-        require(_candidateNames.length >= 2, "no ballot can be created with less than 2 candidates");
+        require(_candidateNames.length >= 2,
+          "no ballot can be created with less than 2 candidates");
 
         owner = msg.sender;
         name = _name;
@@ -42,12 +52,14 @@ contract Ballot {
         endTime = _endTime;
         externalitiesEnabled = _externalitiesEnabled;
         for (uint i = 0; i < _votersCodeHashes.length; i++){
-            require(! m_voterHasNotYetVotedWithCodeHash[_votersCodeHashes[i]], "each voter's hashes must be distinct");
+            require(! m_voterHasNotYetVotedWithCodeHash[_votersCodeHashes[i]],
+              "each voter's hashes must be distinct");
 
             m_voterHasNotYetVotedWithCodeHash[_votersCodeHashes[i]] = true;
         }
         for (uint i = 0; i < _candidateNames.length; i++){
-            require(_candidateNames[i][0] != 0, "every candidate must have a defined name");
+            require(_candidateNames[i][0] != 0,
+              "every candidate must have a defined name");
 
             candidateNames[i] = _candidateNames[i];
             m_candidates[_candidateNames[i]].pictureHash = _pictureHashes[i];
@@ -70,11 +82,14 @@ contract Ballot {
     //public methods
 
     //getters
-    function getCandidateScore(bytes32 _candidateName) public view returns(uint){
-        return m_candidates[_candidateName].poll + m_candidates[_candidateName].externality;
+    function getCandidateScore(bytes32 _candidateName)
+      public view returns(uint){
+        return m_candidates[_candidateName].poll +
+          m_candidates[_candidateName].externality;
     }
 
-    function getCandidateExternality(bytes32 _candidateName) public view returns(uint){
+    function getCandidateExternality(bytes32 _candidateName)
+      public view returns(uint){
         return m_candidates[_candidateName].externality;
     }
 
@@ -82,7 +97,8 @@ contract Ballot {
         return candidateNames;
     }
 
-    function getCandidatePictureHash(bytes32 _candidateName) public view returns(bytes32){
+    function getCandidatePictureHash(bytes32 _candidateName)
+      public view returns(bytes32){
         return m_candidates[_candidateName].pictureHash;
     }
 
@@ -101,19 +117,22 @@ contract Ballot {
     //for voters
 
     //warning: doesn't check if candidate actually exists
-    function vote(bytes32 _candidateName, bytes32 _voterCode) mustBeBeforeEndTime public{
+    function vote(bytes32 _candidateName, bytes32 _voterCode)
+      mustBeBeforeEndTime public{
         bytes32 voterCodeHash = keccak256(abi.encodePacked(_voterCode));
 
         require(m_voterHasNotYetVotedWithCodeHash[voterCodeHash],
-            "you must be allowed to vote at contract creation / you can only vote once");
+            "you must be allowed to vote / can only vote once");
 
         m_candidates[_candidateName].poll++;
         m_voterHasNotYetVotedWithCodeHash[voterCodeHash] = false;
     }
 
     //warning: doesn't check if candidate actually exists
-    function addNewExternality(bytes32 _candidateName, uint _externality) onlyOwner mustBeBeforeEndTime public{
-        require(externalitiesEnabled, "externalities must be enabled at ballot creation");
+    function addNewExternality(bytes32 _candidateName, uint _externality)
+      onlyOwner mustBeBeforeEndTime public{
+        require(externalitiesEnabled,
+          "externalities must be enabled at ballot creation");
 
         m_candidates[_candidateName].externality += _externality;
     }
